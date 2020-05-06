39dcb03059a21adb13e83312b38b2e4030eb4a00
vnkama <220-230@mail.ru>
2020-05-06 23:03:42 +0300

    draw_road

--- MapWnd.py
+++ MapWnd.py
@@ -68,7 +68,7 @@ class MapWnd(GuiWindow):
         )
 
         self.arr_trees.append(
-            Tree(200, 200, arr_groups)
+            Tree(700, 200, arr_groups)
         )
 
         self.arr_trees.append(
@@ -95,6 +95,17 @@ class MapWnd(GuiWindow):
         #формирует дорогу
         self.init_road()
 
+        for section in self.arr_roadsections_corners:
+
+            polygon_points = []
+            for point in section:
+                polygon_points.append((int(point[0]),int(point[1])))
+
+            pg.draw.polygon(
+                    self.background_srf,
+                    (20, 20, 220),
+                    polygon_points
+                )
 
         getMainWnd().registerHandler_MOUSEBUTTONDOWN(self)
         getMainWnd().registerHandler_KEYDOWN(self)
@@ -104,26 +115,11 @@ class MapWnd(GuiWindow):
 
     def init_road(self):
 
-        arr_road_point = [[] * 3]
-
-        # осевая лигния дороги
-        # self.arr_road_centrum_pos = [
-        #     ((0, 200), 100),
-        #     ((100, 100), 100),
-        #     ((200, 200), 100),
-        #     ((300, 300), 100),
-        #     ((350, 400), 100),
-        #     ((450, 430), 100),
-        #     ((550, 430), 100),
-        #     ((650, 420), 100),
-        #     ((680, 360), 50),
-        #     ((690, 260), 50),
-        # ]
-
         self.arr_roadsections_axial_points =  np.array(
             [
                 [0, 200],
                 [100, 100],
+                [150, 100],
                 [200, 200],
                 [300, 300],
                 [350, 400],
@@ -144,117 +140,151 @@ class MapWnd(GuiWindow):
                 100,
                 100,
                 100,
+                100,
 
                 100,
                 100,
-                100,
-                100,
-                100,
+                140,
+                140,
+                140,
             ],
             int
         )
 
 
-        # инициируем массив дороги
-        # self.arr_road_pos - содержит по две точки , левую и правую, на каждом повороте дороги
         road_len = len(self.arr_roadsections_axial_points)
 
 
 
-        #вектора 2d [x y 1] дорога-осевая линия-повороты
-        #indexes 0..road_len
+        #вектора-координаты 2d [x y 1] на повороты осевая линия дороги
+        #индексы 0..road_len
         arr_roadsections_axial_turn_coords = np.zeros((road_len,3),int)
         for i in range(road_len): #
             arr_roadsections_axial_turn_coords[i] = np_d2_getMatrix(self.arr_roadsections_axial_points[i])
 
-            # arr_roadsections_axial_turn_coords[i][0] = self.arr_roadsections_axial_points[i][0]
-            # arr_roadsections_axial_turn_coords[i][1] = self.arr_roadsections_axial_points[i][1]
-            # arr_roadsections_axial_turn_coords[i][2] = 1
+
+        #секция дороги в прмяоугольном виде. 4 угла
+        arr_P = np.zeros((road_len-1,4,3),float)
+
+
+        #нормальные вектора левого-правого краев дороги
+
+        arr_N_left = np.zeros((road_len-1,3),float)
+        arr_A_left = np.zeros((road_len-1),float)
+        arr_B_left = np.zeros((road_len-1),float)
+        arr_C_left = np.zeros((road_len-1),float)
+
+        arr_N_right = np.zeros((road_len-1,3),float)
+        arr_A_right = np.zeros((road_len-1),float)
+        arr_B_right = np.zeros((road_len-1),float)
+        arr_C_right = np.zeros((road_len-1),float)
 
 
 
-        #4 угла секции дороги. секция в прмяоугольном виде
-        self.arr_roadsection_corners = np.zeros((road_len-1,4,3),float)
+        # матрицы поворота
+        matrix_rotate_left = np_d2_getRotateMatrixLeft90()
+        matrix_rotate_right = np_d2_getRotateMatrixRight90()
 
+        self.arr_roadsections_corners = np.zeros((road_len-1,4,3),float)
 
-
-        #вектора-направляения [x y 0], это направления участков дороги.
-        #направления в точке [road_len] нет - некуда направлять
-        arr_roadsections_direction = np.zeros((road_len-1,3),float)
+                            #вектора-направляения [x y 0], это направления участков дороги.
+                            #направления из точки [road_len] нет - некуда направлять
+                            #arr_roadsections_direction = np.zeros((road_len-1,3),float)
         for i in range(road_len-1):
-            arr_roadsections_direction[i] = arr_roadsections_axial_turn_coords[i+1] - arr_roadsections_axial_turn_coords[i]
+            # вектора-направляения [x y 0], направления участков дороги, считается вдоль осевой.
+            roadsectin_axis_vector = arr_roadsections_axial_turn_coords[i+1] - arr_roadsections_axial_turn_coords[i]
 
-            #масштабируем вектор направления
+            #коеффициент масштабирования
             #ширина дороги в начале и конце секции разная
-            k_width_begin = arr_roadsections_width[i] / (2 * np_vector2_len(arr_roadsections_direction[i]))
-            k_width_end = arr_roadsections_width[i+1] / (2 * np_vector2_len(arr_roadsections_direction[i]))
+            k_width_begin = arr_roadsections_width[i] / (2 * np_vector2_len(roadsectin_axis_vector))
+            k_width_end = arr_roadsections_width[i+1] / (2 * np_vector2_len(roadsectin_axis_vector))
 
             #матрица масштабирования начала и конца секции
             matrix_scaling_begin = np_d2_getScaleMatrix(k_width_begin)
             matrix_scaling_end = np_d2_getScaleMatrix(k_width_end)
 
-            #матрицы поворота
-            matrix_rotate_left = np_d2_getRotateMatrixLeft90()
-            matrix_rotate_right = np_d2_getRotateMatrixRight90()
+            #масшиабируем вектор направления до нужной длины
+            roadsection_direction_begin = matrix_scaling_begin @ roadsectin_axis_vector
+            roadsection_direction_end = matrix_scaling_end @ roadsectin_axis_vector
 
-            #масшиабируем вектор направления
-            roadsections_direction_begin = matrix_scaling_begin @ arr_roadsections_direction[i]
-            roadsections_direction_end = matrix_scaling_end @ arr_roadsections_direction[i]
-
-
-
-            #вектора углов секции. прямоугольный формат
+            #arr_P координаты 4х углов секции. прямоугольный формат секции
             #0 - начало секции левый
             #1 - конец секции левый
             #2 - конец секции правый
             #3 - начало секции правый
 
-            # print(matrix_rotate_left)
-            # print(roadsections_direction_begin)
-            # a =  matrix_rotate_left @ roadsections_direction_begin
+            #self - убрать TODO
+            # углы
+            arr_P[i][0] = arr_roadsections_axial_turn_coords[i] + matrix_rotate_left @ roadsection_direction_begin
+            arr_P[i][1] = arr_roadsections_axial_turn_coords[i+1] + matrix_rotate_left @ roadsection_direction_end
+            arr_P[i][2] = arr_roadsections_axial_turn_coords[i+1] + matrix_rotate_right @ roadsection_direction_end
+            arr_P[i][3] = arr_roadsections_axial_turn_coords[i] + matrix_rotate_right @ roadsection_direction_begin
+
+            #нормальные вектора левого-правого краев дороги
+            # края дороги могут быть непаррадедльны осевой
+            N_left = matrix_rotate_left @ (arr_P[i][1] - arr_P[i][0])
+            N_right = matrix_rotate_right @ (arr_P[i][2] - arr_P[i][3])
+
+            #считаем общее уравнение прямой для левого-правого краев дороги
+            #общее уравнение прямой для края дороги
+            # a*x1 + b*y1 + c1 = 0
+            # a = Nx, берем из нормального вектора
+            # b = Ny, берем из нормального вектора
+            # c = -(a x1  + b y1)       x1 y1 - любая точка через которую проходит прямая
+
+            # print(i,arr_N_left[i])
+            # a = arr_N_left[i][0]
+            # arr_A_left[i]=a
+
+            arr_A_left[i] = N_left[0]
+            arr_B_left[i] = N_left[1]
+            arr_C_left[i] = -(arr_A_left[i] * arr_P[i][0][0] +  arr_B_left[i] * arr_P[i][0][1])
+
+            arr_A_right[i] = N_right[0]
+            arr_B_right[i] = N_right[1]
+            arr_C_right[i] = -(arr_A_right[i] * arr_P[i][3][0] +  arr_B_right[i] * arr_P[i][3][1])
+
+        self.arr_roadsections_corners = arr_P
+
+        #просчитаем все реальные углы
+        #в последней секции не просчитываем
+        for i in range(road_len - 1):
+        #     #print(self.arr_roadsections_corners)
+        #
+            if (i == 0):
+                # в 0й сейкции, реальные 0й и 3й углы совпадают с прямоугольными, тк. "минус первой" секции не сущесвует
+                self.arr_roadsections_corners[i][0] = arr_P[i][0]
+                self.arr_roadsections_corners[i][3] = arr_P[i][3]
+            else:
+                #все секции кроме нулевой, копируют 0й 3й углы с предыдущей секции, т.к. они совпадают
+                self.arr_roadsections_corners[i][0] = self.arr_roadsections_corners[i-1][1]
+                self.arr_roadsections_corners[i][3] = self.arr_roadsections_corners[i-1][2]
+
+            if (i == (road_len - 2)):
+                # в последней сейкции, реальные 1й и 2й углы совпадают с прямоугольными, тк. "после последней " секции не сущесвует
+                self.arr_roadsections_corners[road_len - 2][1] = arr_P[road_len - 2][1]
+                self.arr_roadsections_corners[road_len - 2][2] = arr_P[road_len - 2][2]
+            else:
+                #pass
+                #считаем реальные 1й 2й углы секции
+                # self.arr_roadsections_corners[i][1] = arr_P[i][1]
+                # self.arr_roadsections_corners[i][2] = arr_P[i][2]
+                # continue
+                # 1й угол
+
+                 self.arr_roadsections_corners[i][1] = np_d2_getLinesIntersectPoint(
+                     arr_A_left[i], arr_B_left[i], arr_C_left[i],
+                     arr_A_left[i+1], arr_B_left[i+1], arr_C_left[i+1]
+                 )
+
+                 self.arr_roadsections_corners[i][2] = np_d2_getLinesIntersectPoint(
+                     arr_A_right[i], arr_B_right[i], arr_C_right[i],arr_A_right[i+1], arr_B_right[i+1], arr_C_right[i+1]
+                 )
+
+        print(self.arr_roadsections_corners)
 
 
-            self.arr_roadsection_corners[i][0] = arr_roadsections_axial_turn_coords[i]   + matrix_rotate_left    @ roadsections_direction_begin
-            self.arr_roadsection_corners[i][1] = arr_roadsections_axial_turn_coords[i+1] + matrix_rotate_left    @ roadsections_direction_end
-            self.arr_roadsection_corners[i][2] = arr_roadsections_axial_turn_coords[i+1]   + matrix_rotate_right   @ roadsections_direction_end
-            self.arr_roadsection_corners[i][3] = arr_roadsections_axial_turn_coords[i] + matrix_rotate_right   @ roadsections_direction_begin
 
-
-            # print(i,k_width)
-            # print(arr_roadsections_direction[i])
-            # print(arr_roadsections_direction[i],"\r\n")
-
-
-
-        #координаты углов дорожных секций
-
-
-
-
-            # пустой массив
-        # 1й индекс порядковый номер поворота от начала дороги, этот индекс ссответствует индексу в self.arr_road_centrum_pos
-        # 2й индекс, 0-левая сторна дороги, 1-правая сторона дороги
-        # 3й индекс, 0-x, 1-y, собственно координаты точки
-        #self.arr_road_pos = np.zeros((road_len, 2, 2), int)
-
-
-
-
-        # вектора по оси дороги
-        arr_roadsection_vector = np.zeros((road_len, 2), int)
-        arr_central_angle = np.zeros((road_len, 2), float)
-
-        # радиус векторы идут по оси дороги, 0й вектор из 0й точки
-        # из последней точки радиус-вектора нет (т.к. некуда вести)
-        # for i in range(road_len - 1):
-        #     arr_roadsection_vector[i] = vector_getRadiusVector(self.arr_road_centrum_pos[i][0],
-        #                                                        self.arr_road_centrum_pos[i + 1][0])
-
-            # if (i != 0):
-            #     arr_central_angle[i-1] = vector_calcAngle(arr_central_vector[i],arr_central_vector[i-1])
-            #     print(i,arr_central_angle[i])
-
-        # углы поворота дороги, на сколько поворачивается дорга на каждом повороте
 
 
 
@@ -291,43 +321,43 @@ class MapWnd(GuiWindow):
             pg.sprite.collide_mask
         )
 
-        prev_pos = None
-        for i,point_desc in enumerate(self.arr_roadsections_axial_points):
-            cur_pos = point_desc
-
-            if (i != 0 ):
-                pg.draw.line(
-                    self.surface,
-                    (180,80,80),             #цивет
-                    prev_pos,
-                    cur_pos,
-                    2
-                )
-            prev_pos = cur_pos
-
-        for roadsection_corners in self.arr_roadsection_corners:
-
-            points = (
-                (int(roadsection_corners[0][0]),int(roadsection_corners[0][1])),
-                (int(roadsection_corners[1][0]),int(roadsection_corners[1][1])),
-                (int(roadsection_corners[2][0]),int(roadsection_corners[2][1])),
-                (int(roadsection_corners[3][0]),int(roadsection_corners[3][1])),
-            )
-
-
-            pg.draw.polygon(
-                self.surface,
-                (20, 20, 220),
-                points
-            )
-
-            for corner in range(4):
-                pg.draw.circle(
-                    self.surface,
-                    (80, 80, 180),
-                    (int(roadsection_corners[corner][0]),int(roadsection_corners[corner][1])),
-                    2
-                )
+        # prev_pos = None
+        # for i,point_desc in enumerate(self.arr_roadsections_axial_points):
+        #     cur_pos = point_desc
+        #
+        #     if (i != 0 ):
+        #         pg.draw.line(
+        #             self.surface,
+        #             (180,80,80),             #цивет
+        #             prev_pos,
+        #             cur_pos,
+        #             2
+        #         )
+        #     prev_pos = cur_pos
+        #
+        # for roadsection_corners in self.arr_roadsection_corners:
+        #
+        #     points = (
+        #         (int(roadsection_corners[0][0]),int(roadsection_corners[0][1])),
+        #         (int(roadsection_corners[1][0]),int(roadsection_corners[1][1])),
+        #         (int(roadsection_corners[2][0]),int(roadsection_corners[2][1])),
+        #         (int(roadsection_corners[3][0]),int(roadsection_corners[3][1])),
+        #     )
+        #
+        #
+        #     pg.draw.polygon(
+        #         self.surface,
+        #         (20, 20, 220),
+        #         points
+        #     )
+        #
+        #     for corner in range(4):
+        #         pg.draw.circle(
+        #             self.surface,
+        #             (80, 80, 180),
+        #             (int(roadsection_corners[corner][0]),int(roadsection_corners[corner][1])),
+        #             2
+        #         )
 
 
 
--- Vector.py
+++ Vector.py
@@ -80,9 +80,82 @@ def np_d2_getRotateMatrixLeft90():
         float)
 
 
+#детерминант матрицы
+def np_detD2(a):
+    return a[0, 0] * a[1, 1] - a[0, 1] * a[1, 0]
+
 def np_vector2_len(a):
     return math.sqrt(a[0] ** 2 + a[1] ** 2)
 
+#=========================================================
+def np_d2_getLinesIntersectPoint(a1,b1,c1,a2,b2,c2):
+    #ищет точку пересечения двух прямых
+    # a1, b1, c1 -уравнение прямой в общем виде
 
+    print(a1,b1,c1,a2,b2,c2)
+
+    matrix_D = np.array([
+        [a1, b1],
+        [a2, b2],
+    ])
+
+    matrix_Dx = np.array([
+        [-c1, b1],
+        [-c2, b2],
+    ])
+
+    matrix_Dy = np.array([
+        [a1, -c1],
+        [a2, -c2],
+    ])
+
+    D = np_detD2(matrix_D)
+    Dx = np_detD2(matrix_Dx)
+    Dy = np_detD2(matrix_Dy)
+    print(D,Dx,Dy)
+
+    return [Dx / D, Dy / D ,1]
+
+
+
+
+# class DirectLine():
+#
+#     # общее уравнение прямой
+#     # a*x1 + b*y1 + c1 = 0
+#
+#     def setPN(self,P,N):
+#         #P - точка через которую проходит Прямая
+#         #N - нормальнаый вектор прямой. (длинна любая)
+#
+#         self.a = self.Nx = Nx[0]
+#         self.b = self.Ny = Nx[1]
+#         self.c = -(self.a * P[0] + self.b * P[1])       #P[0] = Px,P[1] = Py
+#
+#     def getIntersectPoint(self,line2):
+#         matrix_D = np.array([
+#             [self.a,    self.b],
+#             [line2.a,   line2.b],
+#         ])
+#
+#         matrix_Dx = np.array([
+#             [-self.c,   self.b],
+#             [-line2.c,  line2.b],
+#         ])
+#
+#         matrix_Dy = np.array([
+#             [self.a,    -self.c],
+#             [line2.a,   -line2.c],
+#         ])
+#
+#
+#         D = np_detD2(matrix_D)
+#         Dx = np_detD2(matrix_Dx)
+#         Dy = np_detD2(matrix_Dy)
+#
+#         intersect_x = Dx / D
+#         intersect_y = Dy / D
+#
+#         return [intersect_x,intersect_Y,1]
 
 
